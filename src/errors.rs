0e0981e77252d188ca912e246a38b26c59768a29
gabyga-11 <gabycarniglia@gmail.com>
2023-04-01 19:08:47 -0300

    Agrego errores

--- src/errors.rs
+++ src/errors.rs
@@ -2,7 +2,6 @@ use std::process::exit;
 
 #[derive(Debug)]
 pub enum TypeError{
-    //ErrorPath,
     NombreDeArchivoInvalido,
     NoSeIngresoArgumentoValidoPorConsola,
     AperturaDeArchivoInvalida,
@@ -16,9 +15,24 @@ pub enum TypeError{
     HayDosPiezasNegras,
 }
 
-//TODO: Implementar match con mensaje mas amigable
-
 pub fn catch(tipo: TypeError){
-    println!("ERROR: {:?}",tipo);
+    let mensaje_error = obtener_mensaje_personalizado(tipo);
+    println!("ERROR: {}",mensaje_error);
     exit(-1);
+}
+
+fn obtener_mensaje_personalizado(tipo: TypeError) -> String{
+    match tipo {
+        TypeError::NombreDeArchivoInvalido => String::from("Nombre de Archivo Inválido"),
+        TypeError::NoSeIngresoArgumentoValidoPorConsola => String::from("No se ha ingresado un argumento válido por consola"),
+        TypeError::AperturaDeArchivoInvalida => String::from("Apertura de archivo inválida"),
+        TypeError::TamanioDeTableroIncorrecto => String::from("El tamaño del tablero no es correcto"),
+        TypeError::ArchivoConFormatoDeEspaciosIncorrecta => String::from("El archivo no respeta el formato correcto"),
+        TypeError::ArchivoConCantidadDeCasillerosVaciosIncorrecta => String::from("Cantidad de casilleros vacíos incorrecta"),
+        TypeError::ArchivoConFormatoDeEspaciosImparesIncorrecta => String::from("El archivo no respeta la estructura de espacios entre casillas '_' "),
+        TypeError::CantidadDePiezasIncorrecta => String::from("La cantidad de piezas en el archivo no es la correcta"),
+        TypeError::PiezaInexistenteEnAjedrez => String::from("Se ha ingresado una pieza inexistente en el archivo"),
+        TypeError::HayDosPiezasBlancas => String::from("Hay dos piezas blancas en el tablero"),
+        TypeError::HayDosPiezasNegras => String::from("Hay dos piezas negras en el tablero"),
+    }
 }
